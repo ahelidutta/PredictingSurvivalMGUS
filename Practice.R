cbfb745448097b22b69e2c3f61b5a2e7996a89a5
ahelidutta <61144906+ahelidutta@users.noreply.github.com>
2021-02-18 03:26:35 -0800

    Started on data exploration table

--- Practice.R
+++ Practice.R
@@ -8,12 +8,10 @@ train <- survival::mgus2
 covariates <- c("age", "sex", "hgb", "creat", "mspike", "ptime", "pstat")
 
 #Data censoring
-train$upperBound = ifelse(train$death == 1, train$futime, Inf)
-train
 
 #univariate function for multiple covariates
 univ_formulas <- sapply(covariates,
-                        function(x) as.formula(paste('Surv(upperBound, death)~', x)))
+                        function(x) as.formula(paste('Surv(futime, death)~', x)))
 univ_models <- lapply( univ_formulas, function(x){coxph(x, data = a)})
 signRes <- lapply(univ_models,
                   
@@ -22,7 +20,7 @@ signRes <- lapply(univ_models,
                          x <- summary(x)
                          p.value<-signif(x$wald["pvalue"], digits=2)
                          wald.test<-signif(x$wald["test"], digits=2)
-                         beta <-signif(x$coef[1], digits=2);#coeficient beta
+                         beta <-signif(x$coef[1], digits=2);#coefficient beta
                          HR <-signif(x$coef[2], digits=2);#exp(beta)
                          HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
                          HR.confint.upper <- signif(x$conf.int[,"upper .95"],2)
@@ -38,17 +36,122 @@ res <- t(as.data.frame(signRes, check.names = FALSE))
 as.data.frame(res)
 
 #Multivariable
-significance.cox <- coxph(Surv(upperBound, death) ~ age + sex + hgb + creat + mspike + ptime + pstat, data =  a)
+significance.cox <- coxph(Surv(futime, death) ~ age + sex + hgb + creat + mspike + ptime + pstat, data =  a)
 summary(significance.cox)
 
-#Graphing for hgb
-meanAge <- mean(train$age)
-maxHgb <- max(train$hgb, na.rm=TRUE)
-minHgb <- min(train$hgb, na.rm=TRUE)
-significance.cox <- coxph(Surv(upperBound, death) ~ meanAge + sex + hgb, data =  a)
-ggsurvplot(survfit(significance.cox), data=significance.cox, color = "#2E9FDF",
-           ggtheme = theme_minimal())
-minHgb
-maxHgb
 
+#bin hemoglobin
+train$hgb_bin= cut(train$hgb, breaks=3, labels=FALSE)
+train
+significance.cox <- coxph(Surv(futime, death) ~ age + sex + hgb_bin + creat + mspike + ptime + pstat, data =  train)
+
+
+hgb_df <- with(train,
+               data.frame(hgb_bin = c(1,2,3), 
+                          age = rep(mean(age, na.rm = TRUE), 3),
+                          sex = c("M", "M","M"),
+                          creat = rep(mean(creat, na.rm = TRUE), 3),
+                          mspike = rep(mean(mspike, na.rm = TRUE), 3),
+                          ptime = rep(mean(ptime, na.rm = TRUE), 3),
+                          pstat = c(1, 1,1)
+               )
+)
+
+hgb_df_F <- with(train,
+               data.frame(hgb_bin = c(1,2,3), 
+                          age = rep(mean(age, na.rm = TRUE), 3),
+                          sex = c("F", "F","F"),
+                          creat = rep(mean(creat, na.rm = TRUE), 3),
+                          mspike = rep(mean(mspike, na.rm = TRUE), 3),
+                          ptime = rep(mean(ptime, na.rm = TRUE), 3),
+                          pstat = c(1, 1,1)
+               )
+)
+hgb_df
+
+fit <- survfit(significance.cox, newdata = hgb_df)
+ggsurvplot(fit, conf.int = TRUE, data = hgb_df, legend.labs=c("Low Hemoglobin","Middle Hemoglobin", "High Hemoglobin"),
+           ggtheme = theme_minimal())+ ggtitle("Men")
+ggsurvplot(fit, conf.int = TRUE, data = hgb_df_F, legend.labs=c("Low Hemoglobin","Middle Hemoglobin", "High Hemoglobin"),
+           ggtheme = theme_minimal())+ ggtitle("Women")
+
+#bin age
+train$age_bin= cut(train$age, breaks=3, labels=FALSE)
+train 
+significance.cox <- coxph(Surv(futime, death) ~ age_bin + sex + hgb + creat + mspike + ptime + pstat, data =  train)
+age_df <- with(train,
+                 data.frame(age_bin = c(1,2,3), 
+                            hgb = rep(mean(hgb, na.rm = TRUE), 3),
+                            sex = c("F", "F","F"),
+                            creat = rep(mean(creat, na.rm = TRUE), 3),
+                            mspike = rep(mean(mspike, na.rm = TRUE), 3),
+                            ptime = rep(mean(ptime, na.rm = TRUE), 3),
+                            pstat = c(1, 1,1)
+                 )
+)
+
+age_df_M <- with(train,
+               data.frame(age_bin = c(1,2,3), 
+                          hgb = rep(mean(hgb, na.rm = TRUE), 3),
+                          sex = c("M", "M","M"),
+                          creat = rep(mean(creat, na.rm = TRUE), 3),
+                          mspike = rep(mean(mspike, na.rm = TRUE), 3),
+                          ptime = rep(mean(ptime, na.rm = TRUE), 3),
+                          pstat = c(1, 1,1)
+               )
+)
+age_df_M
+fit <- survfit(significance.cox, newdata = age_df)
+fit_M <- survfit(significance.cox, newdata = age_df_M)
+
+ggsurvplot(fit, conf.int = TRUE, data = age_df, legend.labs=c("Low Age","Middle Age", "High Age"),
+           ggtheme = theme_minimal())+ ggtitle("Female")
+
+ggsurvplot(fit_M, conf.int = TRUE, data = age_df_M, legend.labs=c("Low Age","Middle Age", "High Age"),
+           ggtheme = theme_minimal())+ ggtitle("Male")
+
+#bin mspike
+train$mspike_bin= cut(train$mspike, breaks=3, labels=FALSE)
+train 
+significance.cox <- coxph(Surv(futime, death) ~ age + sex + hgb + creat + mspike_bin + ptime + pstat, data =  train)
+mspike_df <- with(train,
+               data.frame(age = rep(mean(age, na.rm = TRUE), 3), 
+                          hgb = rep(mean(hgb, na.rm = TRUE), 3),
+                          sex = c("F", "F","F"),
+                          creat = rep(mean(creat, na.rm = TRUE), 3),
+                          mspike_bin = c(1,2,3),
+                          ptime = rep(mean(ptime, na.rm = TRUE), 3),
+                          pstat = c(1, 1,1)
+               )
+)
+fit <- survfit(significance.cox, newdata = mspike_df)
+ggsurvplot(fit, conf.int = TRUE, data = mspike_df, legend.labs=c("Low mspike","Middle mspike", "High mspike"),
+           ggtheme = theme_minimal())+ ggtitle("Female")
+
+#bin Creat
+train$creat_bin= cut(train$creat, breaks=3, labels=FALSE)
+train 
+significance.cox <- coxph(Surv(futime, death) ~ age + sex + hgb + creat_bin + mspike + ptime + pstat, data =  train)
+creat_df <- with(train,
+                  data.frame(age = rep(mean(age, na.rm = TRUE), 3), 
+                             hgb = rep(mean(hgb, na.rm = TRUE), 3),
+                             sex = c("M", "M","M"),
+                             creat_bin = c(1,2,3),
+                             mspike = rep(mean(mspike, na.rm = TRUE), 3),
+                             ptime = rep(mean(ptime, na.rm = TRUE), 3),
+                             pstat = c(1, 1,1)
+                  )
+)
+fit <- survfit(significance.cox, newdata = creat_df)
+ggsurvplot(fit, conf.int = TRUE, data = creat_df, legend.labs=c("Low creat","Middle creat", "High creat"),
+           ggtheme = theme_minimal())+ ggtitle("Male")
+
+
+require("survival")
+fit <- survfit(Surv(futime, death) ~ age + sex + hgb + creat + mspike + ptime + pstat, data = train)
+ggsurv <- ggsurvplot(fit, data= train, fun = "event", conf.int = TRUE,
+                     ggtheme = theme_bw())
+ggsurv$plot +theme_bw() + 
+  theme (legend.position = "right")+
+  facet_grid(age ~ hgb)
 
---
+++ Untitled.R
@@ -0,0 +1,46 @@
+install.packages(c("survminer"))
+library("survival")
+library("survminer")
+b <- survival::mgus
+b
+a <- survival::mgus2
+a
+a$death
+res.cox <- coxph(Surv(age, death) ~ sex, data = a)
+res.cox
+summary(res.cox)
+covariates <- c("age", "sex", "hgb", "creat", "mspike", "ptime")
+univ_formulas <- sapply(covariates,
+                        function(x) as.formula(paste('Surv(futime, death)~', x)))
+univ_models <- lapply( univ_formulas, function(x){coxph(x, data = a)})
+# Extract data 
+univ_results <- lapply(univ_models,
+                       function(x){ 
+                         x <- summary(x)
+                         p.value<-signif(x$wald["pvalue"], digits=2)
+                         wald.test<-signif(x$wald["test"], digits=2)
+                         beta<-signif(x$coef[1], digits=2);#coeficient beta
+                         HR <-signif(x$coef[2], digits=2);#exp(beta)
+                         HR.confint.lower <- signif(x$conf.int[,"lower .95"], 2)
+                         HR.confint.upper <- signif(x$conf.int[,"upper .95"],2)
+                         HR <- paste0(HR, " (", 
+                                      HR.confint.lower, "-", HR.confint.upper, ")")
+                         res<-c(beta, HR, wald.test, p.value)
+                         names(res)<-c("beta", "HR (95% CI for HR)", "wald.test", 
+                                       "p.value")
+                         return(res)
+                         #return(exp(cbind(coef(x),confint(x))))
+                       })
+res <- t(as.data.frame(univ_results, check.names = FALSE))
+as.data.frame(res)
+
+res.cox <- coxph(Surv(futime, death) ~ age + sex + hgb + creat + mspike + ptime + pstat, data =  a)
+summary(res.cox)
+
+res.cox <- coxph(Surv(futime, death) ~ age + sex + hgb + creat + mspike + ptime, data =  a)
+summary(res.cox)
+
+ggsurvplot(survfit(res.cox), data=res.cox, color = "#2E9FDF",
+           ggtheme = theme_minimal())
+
+
